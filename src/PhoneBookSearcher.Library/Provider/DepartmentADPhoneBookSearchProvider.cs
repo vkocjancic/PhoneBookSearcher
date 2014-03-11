6b0567e3c2fcc29304d4f0f9ffa1d2ca3327b2c8
Vladimir Kocjancic <kocjancic@hotmail.com>
2014-03-11 15:28:52 +0100

    Added mssing files

---
+++ src/PhoneBookSearcher.Library/Provider/DepartmentADPhoneBookSearchProvider.cs
@@ -0,0 +1,61 @@
+﻿using PhoneBookSearcher.Library.Config;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookSearcher.Library.Provider {
+
+    /// <summary>
+    /// Search provider for querying directory services by department
+    /// </summary>
+    public class DepartmentADPhoneBookSearchProvider : ADPhoneBookSearchProviderBase {
+
+        #region Constructors
+
+        /// <summary>
+        /// Construcotr
+        /// </summary>
+        /// <param name="config">Configuration for directory services</param>
+        public DepartmentADPhoneBookSearchProvider( ADConfiguration config )
+            : base( config ) {
+        }
+
+        #endregion
+
+        #region ADPhoneBookSearchProviderBase methods
+
+        /// <summary>
+        /// Gets entries for name query
+        /// </summary>
+        /// <param name="query">Search query</param>
+        /// <returns>All entries in AD containing query as part of name or username</returns>
+        public override List<PhoneBookSearchResult> GetEntriesForQuery( string query ) {
+            if (string.IsNullOrWhiteSpace( query ))
+                throw new ArgumentNullException( "Query" );
+            var searcher = SetupSearcherForQuery( this.RootEntry, query );
+            var results = searcher.FindAll();
+            return PropagateDirectoryEntryResults( results );
+        }
+
+        /// <summary>
+        /// Crates directory searcher for specified query
+        /// </summary>
+        /// <param name="deRoot">Directory root element</param>
+        /// <param name="query">Query</param>
+        /// <returns>Directory searcher object.</returns>
+        protected override DirectorySearcher SetupSearcherForQuery( DirectoryEntry deRoot, string query ) {
+            var searcher = new DirectorySearcher( deRoot );
+            searcher.PropertiesToLoad.AddRange( new string[] { "cn", "department", "mail", "telephoneNumber" } );
+            searcher.Filter = string.Format( "(&(objectClass=user)(department=*{0}*))",
+                query );
+            return searcher;
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ src/PhoneBookSearcher.Tests/NameADPhoneBookSearchProviderTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhoneBookSearcher.Library.Provider;
+using PhoneBookSearcher.Library.Config;
+
+namespace PhoneBookSearcher.Tests {
+
+    [TestClass]
+    public class NameADPhoneBookSearchProviderTest {
+
+        [TestMethod]
+        [ExpectedException( typeof( ArgumentNullException ) )]
+        public void Constructor_noConfig_throwsArgumentNullException() {
+            var provider = new NameADPhoneBookSearchProvider( null );
+        }
+
+        [TestMethod]
+        [ExpectedException( typeof( ArgumentNullException ) )]
+        public void Constructor_configSet_NoRootEntryUri_throwsArgumentNullException() {
+            var provider = new NameADPhoneBookSearchProvider( new ADConfiguration() );
+        }
+
+        [TestMethod]
+        public void Constructor_configSet_configSet() {
+            var config = new ADConfiguration() {
+                RootEntryUri = new Uri( "LDAP://mycopany.com/DC=mycopmany,DC=com" )
+            };
+            var provider = new NameADPhoneBookSearchProvider( config );
+            Assert.IsNotNull( provider.Configuration );
+        }
+
+        [TestMethod]
+        [ExpectedException( typeof( ArgumentNullException ) )]
+        public void GetEntriesForQuery_queryNull_throwsArgumentNullException() {
+            var config = new ADConfiguration() {
+                RootEntryUri = new Uri( "LDAP://mycopany.com/DC=mycopmany,DC=com" )
+            };
+            var provider = new NameADPhoneBookSearchProvider( config );
+            provider.GetEntriesForQuery( null );
+        }
+
+    }
+
+}
